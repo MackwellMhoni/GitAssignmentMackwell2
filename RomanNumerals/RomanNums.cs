788659a5b164b31f3440f912746278c3473a7d13
Mackwell <mackwellm@gmail.com>
2025-11-11 11:17:34 +0200

    Basic construction - Failed

--- RomanNumerals/RomanNums.cs
+++ RomanNumerals/RomanNums.cs
@@ -1,14 +1,46 @@
 ﻿
+using Microsoft.VisualBasic;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 public class RomanNums
 {
     public static void Main()
     {
-        Console.WriteLine("Hello, World!");
-    }
+		
+	}
 
 	public string Convert(int input)
 	{
-		return "I";
+		var romanNumerals = new[]
+{
+				new { Value = 1000, Symbol = "M" },
+				new { Value = 900,  Symbol = "CM" },
+				new { Value = 500,  Symbol = "D" },
+				new { Value = 400,  Symbol = "CD" },
+				new { Value = 100,  Symbol = "C" },
+				new { Value = 90,   Symbol = "XC" },
+				new { Value = 50,   Symbol = "L" },
+				new { Value = 40,   Symbol = "XL" },
+				new { Value = 10,   Symbol = "X" },
+				new { Value = 9,    Symbol = "IX" },
+				new { Value = 5,    Symbol = "V" },
+				new { Value = 4,    Symbol = "IV" },
+				new { Value = 1,    Symbol = "I" }
+		};
+		string conv = "";
+
+		foreach (var pair in romanNumerals)
+		{
+			if (pair.Value == input)
+			{
+				conv = pair.Symbol;
+			}
+		}
+		return conv;
+
+
+
 	}
 }
    
--- RomanNumeralsConvTest/UnitTest1.cs
+++ RomanNumeralsConvTest/UnitTest1.cs
@@ -10,5 +10,21 @@ namespace RomanNumeralsConvTest
 
 			Assert.Equal("I", convert.Convert(input));
 		}
+
+		[Fact]
+		public void Test_convert_2_and_3()
+		{
+			var convert = new RomanNums();
+			int input = 2;
+			if (input <= 0 || input > 4)
+			{
+				throw new Exception("Needs to be between 1 and 3");
+			}
+			else
+			{
+				Assert.Equal("II", convert.Convert(input));
+			}
+
+		}
 	}
 }
