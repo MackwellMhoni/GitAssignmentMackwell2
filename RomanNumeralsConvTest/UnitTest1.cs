7077e992f3c857c54a91905a42baa7942ec7cb3d
Mackwell <mackwellm@gmail.com>
2025-11-12 15:39:02 +0200

    400 Conversion Test - Passed

--- RomanNumeralsConvTest/UnitTest1.cs
+++ RomanNumeralsConvTest/UnitTest1.cs
@@ -28,6 +28,13 @@ namespace RomanNumeralsConvTest
 				sub.Append(convert.Convert(100));
 				Assert.Equal("XC", sub.ToString());
 			}
+			else if (input < 500)
+			{
+				rem = 500 - input;
+				sub.Append(convert.Convert(rem));
+				sub.Append(convert.Convert(500));
+				Assert.Equal("CD", sub.ToString());
+			}
 			else
 			{
 				Assert.Equal("Try Again", "Try again");
